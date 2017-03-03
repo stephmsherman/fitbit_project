2ffd33d22f6c8c0308977e17ced6effb5ac45054
Stephanie Sherman <stephmsherman@gmail.com>
2017-03-03 16:46:46 -0500

    additions

--- match_day_minute.py
+++ match_day_minute.py
@@ -2,13 +2,23 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 m = pd.read_csv('/Volumes/LabShareFolder/fitbit_project/fitbit_data/minute_data/all_min_to_day.csv')
+m.rename(columns={'activities_steps':'activities_steps_min'},inplace=True)
 d = pd.read_csv('/Volumes/LabShareFolder/fitbit_project/fitbit_data/summary_daily_data/all_summary_data_processed.csv')
 
 b = pd.merge(m,d, on = ['key','date'], how = 'inner')
 
-b['match_steps'] = np.where(b.activities_steps_x == b.activities_steps_y, 'yes','no')
+b = b[b.activities_steps != 0]
+b = b[b.activities_steps_min != 0]
+
+b.describe()
+
+b['match_steps'] = np.where(b.activities_steps_min == b.activities_steps, 'yes','no')
 
 b['match_steps'].value_counts()
+
+b.plot.scatter(x = 'activities_steps_min',y = 'activities_steps') 
+plt.show()
+
 
 nos = b[b.match_steps == 'no']
 
